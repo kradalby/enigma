f0eee08dd97997a2a8cbabf3197fc55fc9a6ae09
Marte Nordrik Hallan <martenordrik@gmail.com>
2016-03-21 22:44:51 +0000

    Move as_html to model, from templatetag

--- src/app/quiz/models.py
+++ src/app/quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from random import shuffle
 
 class TestUnit(models.Model):
     question = models.CharField(max_length = 255, verbose_name = "Question", blank = True)
@@ -8,17 +9,42 @@ class TestUnit(models.Model):
         
     class Meta:
         ordering = ('question',)
+        
+    def as_html(self):
+        return "<h1>THIS MODEL HAS NOT IMPLEMENTED AS_HTML</h1>"
     
 class MultipleChoiceQuestion(TestUnit):
     correct_answer = models.CharField(max_length = 255, verbose_name = "Correct answer")
     wrong_answer_1 = models.CharField(max_length = 255, verbose_name = "Wrong answer")
     wrong_answer_2 = models.CharField(max_length = 255, verbose_name = "Wrong answer")
+    
+    def as_html(self):
+        html = '<div><ul class="list-group">'
+        alternatives = [self.correct_answer, self.wrong_answer_1, self.wrong_answer_2]
+        shuffle(alternatives)
+        
+        for alternative in alternatives:
+            html += """
+            <li class="list-group-item">
+                <input type='radio' name='%s' id="%s"/>
+                <label for='%s'>%s</label>
+                <div class="highlight"></div>
+            </li>
+            """ % (self, alternative, alternative, alternative)
+        html += "</ul></div>"
+        return html
 
 def image_directory_path(instance, filename):
     return 'upload/quiz/{0}/{1}'.format(instance.id, filename)
     
 class MultipleChoiceQuestionWithImage(MultipleChoiceQuestion):
     image = models.ImageField(upload_to=image_directory_path)
+    
+    def as_html(self):
+        html = """
+        <div><img src="%s" /></div>
+        """ % (self.image.url)
+        return super(MultipleChoiceQuestionWithImage, self).as_html() + html
     
 class LandmarkQuestion(TestUnit):
     original_image = models.ImageField(upload_to=image_directory_path)
--- src/app/quiz/templatetags/testunit_as_html.py
+++ src/app/quiz/templatetags/testunit_as_html.py
@@ -1,34 +1,7 @@
 from django import template
-from random import shuffle
-
-from ..models import *
     
-def multiple_choice_as_html(test_unit):
-    html = '<div><ul class="list-group">'
-    alternatives = [test_unit.correct_answer, test_unit.wrong_answer_1, test_unit.wrong_answer_2]
-    shuffle(alternatives)
-    
-    for alternative in alternatives:
-        html += """
-<li class="list-group-item">
-    <input type='radio' name='%s' id="%s"/>
-    <label for='%s'>%s</label>
-    <div class="highlight"></div>
-</li>
-""" % (test_unit, alternative, alternative, alternative)
-    html += "</ul></div>"
-    return html
-    
-def multiple_choice_with_image_as_html(test_unit):
-    return "MULTIPLE CHOICE WITH IMAGE!"
-    
-test_unit_mapping = {
-    MultipleChoiceQuestion: multiple_choice_as_html,
-    MultipleChoiceQuestionWithImage: multiple_choice_with_image_as_html,
-}
-
 register = template.Library()
 
 @register.filter
 def as_html(test_unit):
-    return test_unit_mapping.get(type(test_unit), type(test_unit))(test_unit)
+    return test_unit.as_html()
