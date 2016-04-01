bc7d82c8531e6a0492c39372f35f9da9536eb462
Marte Nordrik Hallan <martenordrik@gmail.com>
2016-04-01 21:20:10 +0000

    Several aestetic things

--- src/app/quiz/admin.py
+++ src/app/quiz/admin.py
@@ -7,6 +7,9 @@ admin.site.unregister(User)
 from .models import *
 
 admin.site.register(Test)
+admin.site.register(MultipleChoiceQuestion)
+admin.site.register(MultipleChoiceQuestionWithImage)
+admin.site.register(LandmarkQuestion)
 
 #admin.site.register(TestResult)
 #admin.site.register(TestUnitResult)
