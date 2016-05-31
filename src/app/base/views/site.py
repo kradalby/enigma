ad049f68cef70b1e894fb8adc064bd19a0c3ce96
Marte Nordrik Hallan <martenordrik@gmail.com>
2016-05-31 11:26:18 +0000

    See only courses you attend

--- src/app/base/views/site.py
+++ src/app/base/views/site.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from app.quiz.models import Test
+from app.course.models import Course
+from app.userprofile.models import UserProfile, UserGroup
 
 @login_required
 def index(request):
+    user = request.user
+    if user.is_superuser:
+        courses = Course.objects.all()
+    else:
+        userprofile = UserProfile.objects.get(user=user)
+        groups = userprofile.groups.all()
+        courses = Course.objects.filter(groups=groups)
     tests = Test.objects.all()
     return render(request, 'base/site/index.html', {
+        "courses" : courses,
         "tests" : tests
     })
