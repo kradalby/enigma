5d8a10682cc5c329be6c9381602066660cc8507a
Marte Nordrik Hallan <martenordrik@gmail.com>
2016-06-09 21:39:37 +0200

    Made postgres and nginx default installed

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,20 +8,19 @@ apt-get install -y virtualenvwrapper           # Venv
 apt-get install -y git                         # In order to pull/push from VM
 apt-get install -y libevent-dev
 apt-get install -y make                        # In order to use Makefile
-apt-get install -y libpq-dev                   # Required for psycopg2 drivers
+apt-get install -y libpq-dev                   # Required for psycopg2 drivers (and potentially postgresql)
 apt-get install -y libjpeg-dev                 # Required for pillow
 apt-get install -y gcc                         # Required for pillow
-#apt-get install -y nginx                       # A proper web server
-#apt-get install -y build-essential             # Required for supervisord
-#apt-get install -y supervisor                  # Run uwsgi in the background
 
 # Upgrade virtualenv to latest version
 sudo pip install virtualenv --upgrade
 
 # If you wish you wish to have postgres installed
-# apt-get install -y libpq-dev                   # Required for postgresql
-# apt-get install -y postgresql                  # Postgresql
-# apt-get install -y postgresql-contrib          # Postgresql
+apt-get install -y postgresql                  # Postgresql
+apt-get install -y postgresql-contrib          # Postgresql
+
+# Nginx
+apt-get install -y nginx                       # A proper web server
 
 # Add vagrant user if it does not exist
 id -u vagrant &>/dev/null || useradd vagrant --home /home/vagrant --shell /bin/bash
