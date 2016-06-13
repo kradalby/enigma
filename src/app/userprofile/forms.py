f123bc801e55204c73d3700963e0cdba18748918
Marte Nordrik Hallan <martenordrik@gmail.com>
2016-06-13 15:30:12 +0000

    Fix username greneration for group without prefix

--- src/app/userprofile/forms.py
+++ src/app/userprofile/forms.py
@@ -55,7 +55,7 @@ class UserGroupForm(ModelForm):
     def save(self, commit=True):
         amount = self.cleaned_data.get('generated_participants_amount')
         prefix = self.cleaned_data.get('generated_participants_prefix')
-        self.instance.prefix = prefix
+        self.instance.prefix = prefix if prefix else self.instance.name
         saved_instance = super(UserGroupForm, self).save(commit=commit)
         if amount and prefix:
             generate_users(amount, saved_instance, prefix)
