0183174393b708263298e2b2e0d286578316dc78
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-06 16:37:19 +0200

    import prod settings as standard

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,14 +1,1 @@
-from settings.base import *
-
-try:
-    from settings.local import *
-except ImportError:
-    print ("""
-          Could not import local settings, if you want to set up
-          local settings for dev run this:
-          $ make dev
-          or
-          $ make prod
-          """)
-    raise ImportError
-    # from settings.development import *
+from settings.production import *
