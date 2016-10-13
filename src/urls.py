67ac897e39aa0412cb0ac43d8ae9b6a2a0cc0c10
Kristoffer Dalby <kradalby@kradalby.no>
2016-10-13 09:52:27 +0200

    poor mans namespacing of api

--- src/urls.py
+++ src/urls.py
@@ -11,21 +11,23 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'course', course_api.CourseViewSet)
-router.register(r'test', quiz_api.TestViewSet)
-router.register(r'testresult', quiz_api.TestResultViewSet)
-router.register(r'testunit', quiz_api.TestUnitViewSet)
-router.register(r'testunitresult', quiz_api.TestUnitResultViewSet)
-router.register(r'mcq', quiz_api.MultipleChoiceQuestionViewSet)
-router.register(r'mcq/image', quiz_api.MultipleChoiceQuestionWithImageViewSet)
-router.register(r'mcq/video', quiz_api.MultipleChoiceQuestionWithVideoViewSet)
-router.register(r'landmarkquestion', quiz_api.LandmarkQuestionViewSet)
-router.register(r'landmarkregion', quiz_api.LandmarkRegionViewSet)
-router.register(r'outline/question', quiz_api.OutlineQuestionViewSet)
-router.register(r'outline/region', quiz_api.OutlineRegionViewSet)
-router.register(r'outline/solution', quiz_api.OutlineSolutionQuestionViewSet)
-router.register(r'user', userprofile_api.UserViewSet)
-router.register(r'userprofile', userprofile_api.UserProfileViewSet)
-router.register(r'usergroup', userprofile_api.UserGroupViewSet)
+
+router.register(r'quiz/test', quiz_api.TestViewSet)
+router.register(r'quiz/testresult', quiz_api.TestResultViewSet)
+router.register(r'quiz/testunit', quiz_api.TestUnitViewSet)
+router.register(r'quiz/testunitresult', quiz_api.TestUnitResultViewSet)
+router.register(r'quiz/mcq', quiz_api.MultipleChoiceQuestionViewSet)
+router.register(r'quiz/mcq/image', quiz_api.MultipleChoiceQuestionWithImageViewSet)
+router.register(r'quiz/mcq/video', quiz_api.MultipleChoiceQuestionWithVideoViewSet)
+router.register(r'quiz/landmarkquestion', quiz_api.LandmarkQuestionViewSet)
+router.register(r'quiz/landmarkregion', quiz_api.LandmarkRegionViewSet)
+router.register(r'quiz/outline/question', quiz_api.OutlineQuestionViewSet)
+router.register(r'quiz/outline/region', quiz_api.OutlineRegionViewSet)
+router.register(r'quiz/outline/solution', quiz_api.OutlineSolutionQuestionViewSet)
+
+router.register(r'userprofile/user', userprofile_api.UserViewSet)
+router.register(r'userprofile/userprofile', userprofile_api.UserProfileViewSet)
+router.register(r'userprofile/usergroup', userprofile_api.UserGroupViewSet)
 
 urlpatterns = [
     url(r'^', include('app.base.urls')),
