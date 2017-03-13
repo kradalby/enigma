e595739e98e77cb047d6b029a4112fe1cf98f6d5
Kristoffer Dalby <kradalby@kradalby.no>
2017-03-13 16:10:59 +0100

    adds admin ui to more models

--- src/app/quiz/admin.py
+++ src/app/quiz/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import OutlineQuestion, OutlineRegion, OutlineSolutionQuestion
+from .models import OutlineQuestion, OutlineRegion, OutlineSolutionQuestion, Test, TestResult, TestUnit, MultipleChoiceQuestion, MultipleChoiceQuestionWithImage, MultipleChoiceQuestionWithVideo, LandmarkQuestion, LandmarkRegion, TestUnitResult
 
 
 class OutlineQuestionAdmin(admin.ModelAdmin):
@@ -18,3 +18,68 @@ class OutlineSolutionQuestionAdmin(admin.ModelAdmin):
 admin.site.register(OutlineQuestion, OutlineQuestionAdmin)
 admin.site.register(OutlineRegion, OutlineRegionAdmin)
 admin.site.register(OutlineSolutionQuestion, OutlineSolutionQuestionAdmin)
+
+
+class TestAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Test, TestAdmin)
+
+
+class TestResultAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TestResult, TestResultAdmin)
+
+
+class TestUnitAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TestUnit, TestUnitAdmin)
+
+
+class MultipleChoiceQuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(MultipleChoiceQuestion, MultipleChoiceQuestionAdmin)
+
+
+class MultipleChoiceQuestionWithImageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(MultipleChoiceQuestionWithImage,
+                    MultipleChoiceQuestionWithImageAdmin)
+
+
+class MultipleChoiceQuestionWithVideoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(MultipleChoiceQuestionWithVideo,
+                    MultipleChoiceQuestionWithVideoAdmin)
+
+
+class LandmarkQuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LandmarkQuestion, LandmarkQuestionAdmin)
+
+
+class LandmarkRegionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LandmarkRegion, LandmarkRegionAdmin)
+
+
+class TestUnitResultAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TestUnitResult, TestUnitResultAdmin)
