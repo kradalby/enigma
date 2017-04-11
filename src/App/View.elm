e317a9f63317a0b5d497cfe8160448576e489e20
Kristoffer Dalby <kradalby@kradalby.no>
2017-04-11 14:20:31 +0200

    adds "correct" html5 tags for header/main/footer

--- src/App/View.elm
+++ src/App/View.elm
@@ -11,18 +11,22 @@ import Lmq.View
 
 root : Model -> Html Msg
 root model =
-    div []
+    body []
         [ viewHeader model
-        , div [ class "container" ]
-            [ case model.global.mode of
-                Main ->
-                    viewModeMenu
+        , main_ []
+            [ div [ class "container" ]
+                [ div [ class "row" ]
+                    [ case model.global.mode of
+                        Main ->
+                            viewModeMenu
 
-                MultipleChoiceQuestions ->
-                    div [] [ Html.map McqMsg (Mcq.View.root model.mcq) ]
+                        MultipleChoiceQuestions ->
+                            div [] [ Html.map McqMsg (Mcq.View.root model.mcq) ]
 
-                LandmarkQuestions ->
-                    div [] [ Html.map LmqMsg (Lmq.View.root model.lmq) ]
+                        LandmarkQuestions ->
+                            div [] [ Html.map LmqMsg (Lmq.View.root model.lmq) ]
+                    ]
+                ]
             ]
         , viewFooter model
         ]
@@ -31,19 +35,23 @@ root model =
 viewModeMenu : Html Msg
 viewModeMenu =
     div []
-        [ button [ class "btn waves-effect waves-light", onClick (ChangeMode MultipleChoiceQuestions) ] [ text "Multiple Choice Questions" ]
+        [ h4 [] [ text "Choose game" ]
+        , button [ class "btn waves-effect waves-light", onClick (ChangeMode MultipleChoiceQuestions) ] [ text "Multiple Choice Questions" ]
         , button [ class "btn waves-effect waves-light", onClick (ChangeMode LandmarkQuestions) ] [ text "Landmark Questions" ]
         ]
 
 
 viewHeader : Model -> Html Msg
 viewHeader model =
-    nav [ class "light-blue lighten-1" ]
-        [ div [ class "nav-wrapper container" ]
-            [ a [ id "logo-container", onClick (ChangeMode Main), class "brand-logo" ] [ text "Enigma" ]
-            , ul [ class "right hide-on-med-and-down" ] [ li [] [ a [] [ text "derp" ] ] ]
-              -- , ul [ class "nav-mobile" ] [ li [] [ a [] [ text "derp" ] ] ]
-              -- , a [ href "#", dataactivates "nav-mobile", class "button-collapse" ] [ i [ class "material-icons" ] [ text "menu" ] ]
+    header []
+        [ nav [ class "light-blue lighten-1" ]
+            [ div [ class "nav-wrapper container" ]
+                [ a [ id "logo-container", onClick (ChangeMode Main), class "brand-logo" ] [ text "Enigma" ]
+
+                -- , ul [ class "right hide-on-med-and-down" ] [ li [] [ a [] [ text "derp" ] ] ]
+                -- , ul [ class "nav-mobile" ] [ li [] [ a [] [ text "derp" ] ] ]
+                -- , a [ href "#", dataactivates "nav-mobile", class "button-collapse" ] [ i [ class "material-icons" ] [ text "menu" ] ]
+                ]
             ]
         ]
 
@@ -51,25 +59,30 @@ viewHeader model =
 viewFooter : Model -> Html Msg
 viewFooter model =
     footer [ class "page-footer orange" ]
-        [ div [ class "footer-copyright" ]
-            [ -- p [] [ text "Made with ", a [ class "orange-text text-lighten-3", href "http://elm-lang.org" ] [ text "Elm" ] ]
-              p []
-                [ text
-                    ("Copyright "
-                        ++ (toString
-                                (case model.global.date of
-                                    Nothing ->
-                                        1337
+        [ div [ class "container" ] [ div [ class "row" ] [] ]
+        , div [ class "footer-copyright" ]
+            [ div [ class "container" ]
+                [ p []
+                    [ text "Made with ", a [ class "orange-text text-lighten-3", href "http://elm-lang.org" ] [ text "Elm" ] ]
+                , p
+                    []
+                    [ text
+                        ("Copyright "
+                            ++ (toString
+                                    (case model.global.date of
+                                        Nothing ->
+                                            1337
 
-                                    Just date ->
-                                        Date.year date
-                                )
-                           )
-                        ++ " "
-                    )
-                , a [ href "https://github.com/freboto", class "orange-text text-lighten-3" ] [ text "Fredrik Borgen Tørnvall" ]
-                , text " and "
-                , a [ href "https://kradalby.no", class "orange-text text-lighten-3" ] [ text "Kristoffer Dalby" ]
+                                        Just date ->
+                                            Date.year date
+                                    )
+                               )
+                            ++ " "
+                        )
+                    , a [ href "https://github.com/freboto", class "orange-text text-lighten-3" ] [ text "Fredrik Borgen Tørnvall" ]
+                    , text " and "
+                    , a [ href "https://kradalby.no", class "orange-text text-lighten-3" ] [ text "Kristoffer Dalby" ]
+                    ]
                 ]
             ]
         ]
